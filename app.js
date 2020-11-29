3df32edf897a83b2678af2bbf3f67edf1fd22afd
mohamedwaleed15 <42620911+mohamedwaleed15@users.noreply.github.com>
2020-11-29 04:17:41 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+const skills=document.querySelector(".skills");
+const container= document.querySelector(".container");
+skills.addEventListener("click",()=>{
+
+container.classList.toggle("active");
+
+
+
+});
